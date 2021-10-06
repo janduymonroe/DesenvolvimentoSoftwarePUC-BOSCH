1cb11e4d06c15073eb320d48dbe7d7a3e6359818
janduymonroe <janduymonroe@gmail.com.br>
2021-10-06 19:32:28 -0300

    add more exercises

---
+++ C#/ConceitosIniciais/AlteracaoVetor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AlteracaoVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor =0, indice = 0;
+            int[] vetor = new int[10];
+            while(true){
+                if(valor == -1){
+                    break;
+                } else {
+                    try{
+                        Console.WriteLine($"O vetor é {String.Join(",",vetor)}");
+                        System.Console.Write("Digite o valor que deseja inserir ou digite -1 para sair: ");
+                        valor = Convert.ToInt32(Console.ReadLine());
+                        System.Console.Write("Em qual indíce deseja inserir o valor: ");
+                        indice = Convert.ToInt32(Console.ReadLine());
+                        vetor[indice] = valor;
+                    } catch (Exception){
+                        Console.WriteLine("Você precisa digitar um número de 0 a 9 para índice.");
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ C#/ConceitosIniciais/ImprimirVetor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ImprimirVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[10];
+            int contador = 0, numero = 0;
+
+            while(contador<9){        
+                Console.Write("Digite um número para compor o vetor: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+                vetor[contador] = numero;
+                contador++;
+            }
+            foreach (int item in vetor){
+                System.Console.WriteLine(item);
+            }
+
+        }
+    }
+}
