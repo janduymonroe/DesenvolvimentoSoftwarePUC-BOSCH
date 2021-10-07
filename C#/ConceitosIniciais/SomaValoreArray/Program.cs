898a28d4540aed17ae9def35b92ad85a13a1380c
janduymonroe <janduymonroe@gmail.com.br>
2021-10-06 22:34:00 -0300

    add final exercises of the initial concepts

---
+++ C#/ConceitosIniciais/SomaValoreArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace SomaValoreArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numeros;
+            Console.WriteLine("Esse programa imprime um array de 5 itens e a soma deles.");
+            Console.Write("Digite uma sequência de 5 números com espaço entre eles: ");
+            numeros = Console.ReadLine().Split();
+            Console.WriteLine(value: $"Seu array é {string.Join(", ", numeros)}, e sua soma é {numeros.Sum(x=>int.Parse(x))}");
+
+        }
+    }
+}
