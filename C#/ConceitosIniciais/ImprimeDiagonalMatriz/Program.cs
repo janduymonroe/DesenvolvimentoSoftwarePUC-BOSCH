d66f7e9e5d0ee9eb3e731f09792b54cef3892f0c
janduymonroe <janduymonroe@gmail.com.br>
2021-10-06 22:14:35 -0300

    add more exercises

---
+++ C#/ConceitosIniciais/ImprimeDiagonalMatriz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ImprimeDiagonalMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esse programa cria uma matriz 4x4 e imprime sua diagonal.");
+            string[] ordem = {"primeira", "segunda", "terceira", "quarta"};
+            int[,] matriz = new int[4,4];
+            string[] linhaProvisoria;
+            int cont1 = 0, cont2 = 0, cont3 = 0;
+            while(true){
+                Console.Write($"Escreva a {ordem[cont1]} linha da matriz, com espaço entre os números: ");
+                cont1++;
+                linhaProvisoria = Console.ReadLine().Split();
+                foreach(string numero in linhaProvisoria){
+
+                    matriz[cont2,cont3] = Convert.ToInt32(numero);
+                    cont3++;
+                }
+                cont2++;
+                cont3 = 0;
+                if (cont1 == 4){
+                    break;
+                }
+            }
+            
+            Console.WriteLine("A diagonal da matriz é: ");
+            Console.Write($"{matriz[0,0]}\n\t{matriz[1,1]}\n\t\t{matriz[2,2]}\n\t\t\t{matriz[3,3]}");
+            
+        }
+    }
+}
---
+++ C#/ConceitosIniciais/VerificarMegaSena/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace VerificarMegaSena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numerosSorteados = {15, 37, 45, 60, 17, 05};
+            int[] numerosEscolhidos = new int[6];
+            Console.Write("Digite os números escolhidos separados por espaço: ");
+            string[] recebeNumeros = Console.ReadLine().Split();
+            int contador = 0;
+            foreach(string item in recebeNumeros){
+                numerosEscolhidos[contador] = Convert.ToInt32(item);
+            }
+            if (numerosSorteados.Any(item => numerosEscolhidos.Contains(item))){
+                System.Console.WriteLine("Você é um novo milionário.");
+            } else {
+                System.Console.WriteLine("Você precisa de mais sorte.");
+            }
+        }
+    }
+}
